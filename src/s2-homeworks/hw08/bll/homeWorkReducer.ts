733770e5796fa8354fac9f241def492bc51eedc4
Richard Leshik <richardleshik@gmail.com>
2023-03-04 20:29:03 +0300

    add HW8

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,37 @@
 import {UserType} from '../HW8'
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: 'sort'; payload: 'up' | 'down' }
+  | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => {
+  switch (action.type) {
+    case 'sort': {
+      const sortUp = (a: UserType, b: UserType) => {
+        if (a.name < b.name) {
+          return -1;
         }
-        case 'check': {
-
-            return state // need to fix
+        if (a.name > b.name) {
+          return 1;
         }
-        default:
-            return state
+        return 0;
+      }
+      const sortDown = (a: UserType, b: UserType) => {
+        if (a.name < b.name) {
+          return 1;
+        }
+        if (a.name > b.name) {
+          return -1;
+        }
+        return 0;
+      }
+
+      return action.payload === 'up' ? state.sort(sortUp) : state.sort(sortDown)
     }
+    case 'check': {
+      return state.filter(u => u.age >= 18)
+    }
+    default:
+      return state
+  }
 }
