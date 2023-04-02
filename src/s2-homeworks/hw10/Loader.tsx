5b89124729e53e9b7d485424be089a740ddaa737
Richard Leshik <richardleshik@gmail.com>
2023-04-02 13:33:20 +0300

    add HW10

--- src/s2-homeworks/hw10/Loader.tsx
+++ src/s2-homeworks/hw10/Loader.tsx
@@ -1,3 +1,12 @@
 import s from './Loader.module.css'
 
-export const Loader = () => <div className={s.loader}/>
+export const Loader = () => <div className={s.ldsRoller}>
+  <div/>
+  <div/>
+  <div/>
+  <div/>
+  <div/>
+  <div/>
+  <div/>
+  <div/>
+</div>
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -1,22 +1,28 @@
-const initState = {
-    isLoading: false,
+export type StateType = {
+  isLoading: boolean
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
-    switch (action.type) {
-        // пишет студент  // need to fix
-
-        default:
-            return state
-    }
+export type LoadingActionType = {
+  type: 'CHANGE_LOADING'
+  isLoading: boolean
 }
 
-type LoadingActionType = {
-    type: 'CHANGE_LOADING'
-    isLoading: boolean
-}
+export type ActionType = LoadingActionType
 
 export const loadingAC = (isLoading: boolean): LoadingActionType => ({
-    type: 'CHANGE_LOADING',
-    isLoading,
+  type: 'CHANGE_LOADING',
+  isLoading,
 })
+
+const initState: StateType = {
+  isLoading: false,
+}
+
+export const loadingReducer = (state = initState, action: ActionType): StateType => { // fix any
+  switch (action.type) {
+    case 'CHANGE_LOADING':
+      return {...state, isLoading: action.isLoading}
+    default:
+      return state
+  }
+}
