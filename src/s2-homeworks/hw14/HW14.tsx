a431c47f44490bd6fa7be790f77b4f07072da413
Richard Leshik <richardleshik@gmail.com>
2023-05-17 22:32:26 +0300

    add HW14

--- src/s2-homeworks/hw14/HW14.tsx
+++ src/s2-homeworks/hw14/HW14.tsx
@@ -5,13 +5,6 @@ import axios from 'axios'
 import SuperDebouncedInput from './common/c8-SuperDebouncedInput/SuperDebouncedInput'
 import {useSearchParams} from 'react-router-dom'
 
-/*
-* 1 - дописать функцию onChangeTextCallback в SuperDebouncedInput
-* 2 - дописать функцию sendQuery в HW14
-* 3 - дописать функцию onChangeText в HW14
-* 4 - сделать стили в соответствии с дизайном
-* 5 - добавить HW14 в HW5/pages/JuniorPlus
-* */
 
 const getTechs = (find: string) => {
   return axios
@@ -34,22 +27,14 @@ const HW14 = () => {
     setLoading(true)
     getTechs(value)
       .then((res) => {
-        // делает студент
-
-        // сохранить пришедшие данные
-
-        //
+        res && setTechs(res.data.techs)
+        setLoading(false)
       })
   }
 
   const onChangeText = (value: string) => {
     setFind(value)
-    // делает студент
-
-    // добавить/заменить значение в квери урла
-    // setSearchParams(
-
-    //
+    setSearchParams(value)
   }
 
   useEffect(() => {
